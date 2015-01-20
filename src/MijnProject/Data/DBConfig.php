59943f7a4a648f120d87fd52893350e13eaa4b52
karelVanGeerdeghom <karel.van.geerdeghom@gmail.com>
2015-01-20 14:51:32 +0100

    Full Commit

---
+++ src/MijnProject/Data/DBConfig.php
@@ -0,0 +1,7 @@
+<?php
+namespace MijnProject\Data;
+class DBConfig {
+    public static $DB_CONNSTRING = "mysql:host=fdb13.biz.nf; dbname=1790700_me";
+    public static $DB_USER = "1790700_me";
+    public static $DB_PASS = "123never@";
+}
